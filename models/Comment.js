a425d1a0df7182f056f40937e8d475d430c10712
Katherine Kelly <ktkelly11@gmail.com>
2024-06-22 16:02:41 -0400

    Comment model issues solved

--- models/Comment.js
+++ models/Comment.js
@@ -3,20 +3,26 @@ const { Schema, model } = require("../config/db-connection");
 const commentsSchema = Schema({
   name: {
     type: String,
+    required: true,
   },
   title: {
     type: String,
+    required: true,
   },
   comment: [
     {
       content: String,
+      // app crashed
+      // required: true,
     },
   ],
   city: {
     type: String,
+    required: true,
   },
   state: {
     type: String,
+    required: true,
   },
 });
 
